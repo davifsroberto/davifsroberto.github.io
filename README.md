27feb01e57abfc16a5c6aec04aaa33953e678505
Davi Roberto <daviroberto11@hotmail.com>
2022-03-31 23:09:13 -0300

    boilerplate start

---
+++ README.md
@@ -0,0 +1,5 @@
+# Davi Roberto - Frontend Developer
+
+<a href="https://davifsroberto.github.io" target="_blank">
+  <img src="https://raw.githubusercontent.com/davifsroberto/davifsroberto.github.io/master/images/davifsroberto.png" />
+</a>
---
+++ src/styles/global.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #__next {
+    height: 100%;
+  }
+
+  body {
+    font-family: 'Poppins', sans-serif;
+  }
+`;
+
+export default GlobalStyles;
