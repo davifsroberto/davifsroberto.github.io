27b0f94f05ce1a9637a4c5e94e989a3732b54a4c
Davi Roberto <daviroberto11@hotmail.com>
2022-04-08 10:43:51 -0300

    config envs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === "production"
+
+
 const nextConfig = {
   reactStrictMode: true,
   basePath: '',
-  assetPrefix: '/',
+  assetPrefix: isProd ? "/davifsroberto.github.io/" : "",
   images: {
     loader: 'akamai',
     path: '',
