247c9d9a3c7012150e84d8093f59575841d21bc6
davifsroberto <daviroberto11@hotmail.com>
2022-04-09 01:44:58 +0000

    Deploying to gh-pages from @ davifsroberto/davifsroberto.github.io@350da91f222863799e4149cb30669d57c63b1a2b 🚀

--- _next/static/YNWz_8_Ak6dFmF1EkwoI1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/427-9b1c3cf84cdbef70.js","static/css/23eae5b74179e2c4.css","static/chunks/pages/index-5ac9c0f625f39f83.js"],"/_error":["static/chunks/pages/_error-427958541d1e32b7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
