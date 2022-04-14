6309d02a8d199a8b888ccc42254766f0ebc3f4e9
Davi Roberto <daviroberto11@hotmail.com>
2022-04-14 11:55:27 -0300

    animations

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -54,7 +54,8 @@ export function Contact() {
 
       <div className="container">
         <p className="paragraph">Contato</p>
-        <h3 className="mb-sm-5 mb-4">Fique a vontade para entrar em contato</h3>
+
+        <h3 className="mb-sm-5 mb-4">Aguardo o seu contato😉</h3>
 
         <article className="row">
           <section className="col-md-6 text-center">
--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { Container } from './styles';
 import { TimelineType } from '../../types/timeline.type';
 
