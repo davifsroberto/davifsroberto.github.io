b44a8c7fee80d8d8d6f6828852a13328574de223
Davi Roberto <daviroberto11@hotmail.com>
2022-04-14 16:51:00 -0300

    new logo

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -6,18 +6,18 @@ export const Container = styled.header`
   z-index: 2;
 
   .navbar {
-    padding: 0.73em 2em;
+    padding: 1.03em 2em;
     border-bottom: 1px solid rgba(243, 242, 242, 0.41);
   }
 `;
 
-export const Logo = styled.strong`
-  @font-face {
-    font-family: 'Firsta';
-    src: url('/assets/fonts/Firsta.ttf');
-  }
-
-  color: #fff;
-  font-size: 3rem;
-  font-family: 'Firsta';
+export const Logo = styled.h1`
+  background: url('/assets/images/general/davifsroberto-logo.png') no-repeat
+    center;
+  background-size: contain;
+  cursor: pointer;
+  height: 2em;
+  width: 2.8em;
+  margin-bottom: 0;
+  font-size: 2em;
 `;
