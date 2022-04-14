1a86f48b9460b028b2d18d252809b40706cf38f1
Davi Roberto <daviroberto11@hotmail.com>
2022-04-14 12:47:56 -0300

    analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,8 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
+import { GA_TRACKING_ID } from '../lib/gtag';
+
+const isProduction = process.env.NODE_ENV === 'production';
 export default class MyDocument extends Document {
   render() {
     return (
@@ -7,6 +10,28 @@ export default class MyDocument extends Document {
         <title>Davi Roberto</title>
 
         <Head>
+          {isProduction && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
+            </>
+          )}
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
