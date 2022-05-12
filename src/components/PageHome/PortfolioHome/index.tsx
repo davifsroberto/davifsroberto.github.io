3eb18cede339bf6e58d6c9883fa91c09541fab94
Davi Roberto <daviroberto11@hotmail.com>
2022-05-11 21:29:32 -0300

    internalization -  experience, callToAction, portfolio

--- src/components/PageHome/PortfolioHome/index.tsx
+++ src/components/PageHome/PortfolioHome/index.tsx
@@ -3,13 +3,15 @@ import Link from 'next/link';
 import { Container } from './styles';
 import { Portfolio } from '../../Portfolio';
 import { mountPortfolioHomeHelper } from '../home.helper';
+import { SetLanguageUtils as language } from '../../../utils/language.utils';
+import portfolio from '../../../locales/home/portfolio/portfolio.json';
 
 export function PortfolioHome() {
   return (
     <Container id="portfolio" className="pt-5">
       <div className="container">
-        <p className="paragraph">Portfólio</p>
-        <h3 className="mb-sm-5 mb-4">Projetos Recentes</h3>
+        <p className="paragraph">{portfolio[language()].title}</p>
+        <h3 className="mb-sm-5 mb-4">{portfolio[language()].subTitle}</h3>
 
         <div className="container">
           <Portfolio gallery={mountPortfolioHomeHelper()} />
@@ -18,7 +20,7 @@ export function PortfolioHome() {
         <section className="text-center mt-5">
           <Link href="/portfolio" passHref>
             <button className="btn btn-default btn-default-big">
-              VER PORTFÓLIO COMPLETO &nbsp;
+              {portfolio[language()].viewAll} &nbsp;
               <i className="fa fa-arrow-right" />
             </button>
           </Link>
