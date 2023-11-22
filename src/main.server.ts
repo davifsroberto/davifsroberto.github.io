5dc7a574c81e3109d4a7005a7114abeb365b4548
Davi Roberto <daviroberto11@hotmail.com>
2023-11-22 17:35:49 -0300

    pre-push amd build config

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { ApplicationRef } from '@angular/core';
+
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
-import { ApplicationRef } from '@angular/core';
 
 const bootstrap = (): Promise<ApplicationRef> => {
   return bootstrapApplication(AppComponent, config);
