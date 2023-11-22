de2d34a5bf2c691bad87aa854852bfa6ca56dc04
Davi Roberto <daviroberto11@hotmail.com>
2023-11-22 15:03:51 -0300

    prettier config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,5 +5,5 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration()],
 };
