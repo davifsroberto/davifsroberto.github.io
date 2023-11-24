ec159793ffaa77b1c0ed3aaf04f697747e49f216
Davi Roberto <daviroberto11@hotmail.com>
2023-11-24 11:57:44 -0300

    includ angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideRouter } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()],
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimations()],
 };
